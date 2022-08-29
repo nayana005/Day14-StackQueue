938442aa4be5175925c300185372dfecbf53d1b2
nayana005 <nayanasuresh1005@gmail.com>
2022-08-30 01:08:11 +0530

    Added: DeQueue

--- src/com/bridgelabz/Queue.java
+++ src/com/bridgelabz/Queue.java
@@ -1,8 +1,9 @@
 package com.bridgelabz;
 
 public class Queue <T> {
-    MyLinkedList<T> linkedList;
 
+    MyLinkedList<T> linkedList;
+    
     public Queue() {
         this.linkedList = new MyLinkedList<>();
     }
@@ -13,4 +14,17 @@ public class Queue <T> {
     public void print() {
         linkedList.print();
     }
+
+    public boolean isEmpty(){
+        return linkedList.size() == 0;
+    }
+
+    public T peek(){
+        return linkedList.peek();
+    }
+
+    public T dequeue() {
+        return linkedList.pop();
+    }
 }
+
